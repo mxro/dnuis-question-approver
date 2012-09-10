5351d890edcfdb77d11c0e0c00610ff35e553dbc
commit <commit@noemail.com>
2012-09-10 17:48:24 +1200

    fixing small issues

--- 0.0.1/dnuis-question-approver-app-js-0.0.1.js
+++ 0.0.1/dnuis-question-approver-app-js-0.0.1.js
@@ -2,127 +2,181 @@
 
 (function($, AJ) {
 
-		$.initQuestionApprover = function(params) {
+	$.initQuestionApprover = function(params) {
 
-			var elem = params.elem;
-			var client = params.client;
-			
-			var questionsForReviewNode = client.reference("http://slicnet.com/questio/questio");
-			var questionsForReviewSecret = "thd3pb41jrke83i";
-			
-			
-			
-			// constants
-			var aQuestionBag = client.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag");
-			var aQuestionBagRepository = client.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag_Repository");
-			
-			var qa = {};
-			
-			qa.selectedRepo = null;
-			qa.selectedQuestionBag = null;
-			
-			
-			
-			qa.uploadQuestions = function(resolvedQuestionNodes) {
-				client.seed({
-					onSuccess: function(res) {
-						
-						var repo = res.root;
-						
-						client.append({
-							node: aQuestionBagRepository,
-							to: repo
-						});
-						
-						var questions = client.append({
-							node: "questions",
-							to: repo,
-							atAddress: "./questions"
-						});
-						
-						client.append({
-							node: aQuestionBag,
-							to: questions
-						});
-						
-						$.each(resolvedQuestionNodes, function(index, value) {
-							if (value.value) {
-								$('.pendingQuestions').append("<p>"+value.value()+"</p>");
-								
-								var split=value.value().split("&");
-								$('.pendingQuestions').append("<p>Only Url: "+split[0]+"</p>");
-								
-								if (split[0]) {
-									client.append({
-										node: client.reference(split[0]),
-										to: questions
-									});
-								}
-							}
-						});
-						
-						$('.pendingQuestions').append("<p>Uploading ...</p>");
-						
-						client.commit({
-							onSuccess: function() {
-								$('.pendingQuestions').append("<p>All uploaded to: "+repo.url()+" "+res.secret+"</p>");
-							}
-							
-						});
-						
-					}
-				});
-				
-				
-			};
-			
-			qa.updateDestination = function() {
-				var repo = $('.repositorySelect', elem).val();
-				qa.selectedRepo = repo;
-				
-				$('.destRepoLabel', elem).attr('href', repo);
-				$('.destRepoLabel', elem).html(repo);
-				
-				var qbag = $('.questionBagSelect', elem).val();
-				qa.selectedQuestionBag = qbag;
-				$('.questionBagLabel').attr('href', qbag);
-				$('.questionBagLabel').html(qbag);
-			};
-			
-			qa.priv = {};
-			
-			qa.priv.loadQuestions = function() {
-				client.load({
-					node: questionsForReviewNode,
-					secret: questionsForReviewSecret,
-					onSuccess: function(res) {
-						
-						client.select({
-							from: res.loadedNode,
-							onSuccess: function(sr) {
-								
-								uploadQuestions(sr);
-								
-							}
-						});
-						
-					}
-				});
-			};
-			
-			// init ui
-			(function() {
-				qa.updateDestination();
-				
-				
-				elem.show()
-			}) ();
-			
-			return {
+		var elem = params.elem;
+		var client = params.client;
 
-			};
+		var questionsForReviewNode = client
+				.reference("http://slicnet.com/questio/questio");
+		var questionsForReviewSecret = "thd3pb41jrke83i";
+
+		// constants
+		var aQuestionBag = client
+				.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag");
+		var aQuestionBagRepository = client
+				.reference("http://slicnet.com/mxrogm/mxrogm/apps/nodejump/docs/8/n/Types/Question_Bag_Repository");
+
+		var questionFormTemplate = client
+				.reference("https://u1.linnk.it/qc8sbw/usr/apps/textsync/docs/approve-strategy-question-form-html-0.0.1");
+
+		var qa = {};
+
+		qa.selectedRepo = null;
+		qa.selectedQuestionBag = null;
+
+		qa.uploadQuestions = function(resolvedQuestionNodes) {
+			client.seed({
+				onSuccess : function(res) {
+
+					var repo = res.root;
+
+					client.append({
+						node : aQuestionBagRepository,
+						to : repo
+					});
+
+					var questions = client.append({
+						node : "questions",
+						to : repo,
+						atAddress : "./questions"
+					});
+
+					client.append({
+						node : aQuestionBag,
+						to : questions
+					});
+
+					$('.pendingQuestions').append("<p>Uploading ...</p>");
+
+					client.commit({
+						onSuccess : function() {
+							$('.pendingQuestions').append(
+									"<p>All uploaded to: " + repo.url() + " "
+											+ res.secret + "</p>");
+						}
+
+					});
+
+				}
+			});
+
 		};
 
-	})(jQuery, AJ);
+		qa.updateDestination = function() {
+			var repo = $('.repositorySelect', elem).val();
+			qa.selectedRepo = repo;
+
+			$('.destRepoLabel', elem).attr('href', repo);
+			$('.destRepoLabel', elem).html(repo);
+
+			var qbag = $('.questionBagSelect', elem).val();
+			qa.selectedQuestionBag = qbag;
+			$('.questionBagLabel').attr('href', qbag);
+			$('.questionBagLabel').html(qbag);
+		};
+
+		qa.priv = {};
+
+		qa.priv.loadQuestions = function() {
+			client
+					.load({
+						node : questionsForReviewNode,
+						secret : questionsForReviewSecret,
+						onSuccess : function(res) {
+
+							client
+									.select({
+										from : res.loadedNode,
+										onSuccess : function(sr) {
+
+											var num = 1;
+											$
+													.each(
+															sr.values,
+															function(index,
+																	value) {
+																if (value.value) {
+
+																	var split = value
+																			.value()
+																			.split(
+																					"&");
+
+																	if (split[0]
+																			&& split[1]) {
+																		var address = split[0];
+																		var secret = split[1];
+
+																		var newRow = $(
+																				'.incomingQuestions',
+																				elem)
+																				.append(
+																						"<tr><td>1</td><td class='approvalForm'></td>");
+
+																		qa.priv
+																				.appendQuestionForm($(
+																						'.approvalForm',
+																						newRow));
+
+																	}
+																}
+															});
+
+										}
+									});
+
+						}
+					});
+		};
+
+		qa.priv.appendQuestionForm = function(toElem) {
+
+			client.load({
+				node : questionFormTemplate,
+				onSuccess : function(res) {
+
+					var html = client.dereference({
+						ref : res.loadedNode
+					}).value();
+
+					var formElem = toElem.append("<div></div>");
+
+					formElem.html(html);
+
+					var questionForm = $.initStrategyQuestionForm({
+						elem : $('.questionForm', formElem)
+					});
+
+					$('.approveButton', formElem).click(function(evt) {
+						evt.preventDefault();
+						formElem.remove();
+					});
+
+					$('.rejectButton', formElem).click(function(evt) {
+						evt.preventDefault();
+						formElem.remove();
+					});
+
+				}
+			});
+
+		};
+
+		// init ui
+		(function() {
+			qa.updateDestination();
+			
+			qa.priv.loadQuestions();
+			
+			elem.show()
+		})();
+
+		return {
+
+		};
+	};
+
+})(jQuery, AJ);
 
 // <!-- one.end -->
